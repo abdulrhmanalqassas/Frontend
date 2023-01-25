85bd06505cac6d8c60aa3ec33b2629ffdca20836
abdulrhmanalqassas <46942535+abdulrhmanalqassas@users.noreply.github.com>
2023-01-25 22:18:40 +0200

    build

--- src/components/State.js
+++ src/components/State.js
@@ -1,6 +1,5 @@
 import { useRef, useState, useEffect } from "react";
 import axios from "../api/axios";
-import QRCode from "react-qr-code";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import Nav from "./Nav";
 export default function Verifi() {
